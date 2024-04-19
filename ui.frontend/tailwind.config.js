8c6a07176455c91c5d4a5d2f24778dafe80c6b62
Balan Alexandru <indulgentul@gmail.com>
2024-04-20 02:33:12 +0300

    Installed Tailwindcss, untrack clientlibs

---
+++ ui.frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "../ui.apps/src/main/content/jcr_root/apps/pcdepo/components/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
